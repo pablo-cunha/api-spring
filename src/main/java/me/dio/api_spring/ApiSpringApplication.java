6ac7b0d1f481294000b41e295913b5482a87531a
pablo-cunha <lcunha.pablo@gmail.com>
2025-03-28 16:31:31 -0300

    inicializando projeto

---
+++ src/main/java/me/dio/api_spring/ApiSpringApplication.java
@@ -0,0 +1,13 @@
+package me.dio.api_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringApplication.class, args);
+	}
+
+}
